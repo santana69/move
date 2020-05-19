4a3deccd5f8082b8773ea9592827275065de916a
Andres Santana <santanag.andres@gmail.com>
2020-05-18 18:50:03 -0700

    Initial Commit

---
+++ MoveTechnicalActivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveTechnicalActivity
+//
+//  Created by Andres Santana on 18/05/20.
+//  Copyright © 2020 Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
